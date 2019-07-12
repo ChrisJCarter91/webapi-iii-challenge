bff14535863fd660185bd671c1c4d489142b8fd3
chris.j.carter <chris.j.carter91@gmail.com>
2019-07-11 19:13:20 -0600

    Test for heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const server = require('./server');
 
-const port = process.env.PORT || 6000
+const port = process.env.PORT || 6000;
 server.listen(4000, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
